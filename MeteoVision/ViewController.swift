a38ce41257b34faa2f75ce7a9bbc7dec2436e763
Evgeny Levin <evgenylevin91@gmail.com>
2024-03-04 23:05:32 +0100

    Initial Commit

---
+++ MeteoVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeteoVision
+//
+//  Created by YAUHENI LEVIN on 4.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
