32f1b023c22ed7f6f541143f90749c5cf3705e69
Evgeny Levin <evgenylevin91@gmail.com>
2024-03-17 09:33:14 +0100

    Added cache method

--- MVWeatherCacheResponse/Sources/MVWeatherCacheResponse/MVWeatherCacheResponse.swift
+++ MVWeatherCacheResponse/Sources/MVWeatherCacheResponse/MVWeatherCacheResponse.swift
@@ -4,11 +4,48 @@
 //  Created by YAUHENI LEVIN on 17.03.24.
 //
 
+import MVAPIClient
+import Foundation
 
-protocol MWWeatherCacheProviding {
-
+public enum MVWeatherCacheError: Error {
+  case cannotEncodeModel
+  case noDocumentsFolderFound
+  case cannotSaveModel
 }
 
-public final class MWWeatherCacheProvider: MWWeatherCacheProviding {
+public enum MVWeatherCacheResponse {
+  case weather
+  case pollution
+  
+  var cacheFileName: String {
+    return switch self {
+    case .weather:
+      "Current_Weather_Response"
+    case .pollution:
+      "Air_Pollution_Response"
+    }
+  }
+}
 
+public protocol MVWeatherCacheProviding {
+  func cache<T: Encodable>(_ response: T, type: MVWeatherCacheResponse) throws
+}
+
+public final class MVWeatherCacheProvider: MVWeatherCacheProviding {
+  public func cache<T: Encodable>(_ response: T, type: MVWeatherCacheResponse) throws {
+    do {
+      let jsonData = try JSONEncoder().encode(response)
+      guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+        throw MVWeatherCacheError.noDocumentsFolderFound
+      }
+      let fileURL = documentsDirectory.appendingPathComponent("\(type.cacheFileName).json")
+      do {
+        try jsonData.write(to: fileURL)
+      } catch {
+        throw MVWeatherCacheError.cannotSaveModel
+      }
+    } catch {
+      throw MVWeatherCacheError.cannotEncodeModel
+    }
+  }
 }
